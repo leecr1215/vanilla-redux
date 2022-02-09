52d3da0214901e70fc33ada3ff4d4178c0652127
leecr1215 <leecr1215@naver.com>
2022-02-09 17:35:08 +0900

    finish #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 // reducer는 무조건 함수여야함
-const CountModifier = (count = 0) => {
+const CountModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  }
   return count;
 };
 
 const CountStore = createStore(CountModifier); // 데이터 저장하는 곳
+
+CountStore.dispatch({ type: "ADD" });
+CountStore.dispatch({ type: "ADD" });
+CountStore.dispatch({ type: "MINUS" });
 
 console.log(CountStore.getState());
